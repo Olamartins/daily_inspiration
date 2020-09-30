cce61c826321f5a398f44d2b7e6624a9ff2166ac
Olamartins <51275229+Olamartins@users.noreply.github.com>
2020-09-30 07:55:05 +0100

    Update connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -1,11 +1,11 @@
 <?php 
 // Setting up the database:
-$localhost  = "localhost";
-$user       = "root";
-$password   = "";
+$localhost  = "sm9j2j5q6c8bpgyq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$user       = "xsxkyrw3rgisfats";
+$password   = "dz2z1y8qiy5ozmd6";
 
 // Database:
-$db     = "diary";
+$db     = "kzzu1azz2nn5wxqq";
 
 $conn = mysqli_connect($localhost, $user, $password) or die("Connection failed!, Please try again later");
 mysqli_select_db($conn, $db) or die("Could not connect to database!, Please try again later");
